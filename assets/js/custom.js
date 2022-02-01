1ee089064ca8e264b4967c1b19d1550f5afaf8b8
hmikki <30619974+hmikki@users.noreply.github.com>
2022-02-01 12:44:24 +0200

    uploaded

---
+++ assets/js/custom.js
@@ -0,0 +1,23 @@
+function ImageViewCrud(input,name) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            $('#Image-crud-'+name)
+                .attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+};
+function VideoViewCrud(input,name) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            $('#Image-crud-'+name)
+                .attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+};
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
